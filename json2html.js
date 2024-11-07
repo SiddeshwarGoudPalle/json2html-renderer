9feb156cdbe944a2ef5b3f5e3f0d0aacbf1e1769
SIDDESHWAR369 <ugs21052_cic.siddeshwar@cbit.org.in>
2024-11-07 19:01:20 +0530

    Final Code

--- json2html.js
+++ json2html.js
@@ -1,18 +1,14 @@
 export default function json2html(data) {
-    // Get all unique keys from data array to form table headers
     const headers = Array.from(new Set(data.flatMap(Object.keys)));
 
-    // Start building the HTML string
     let html = `<table data-user="siddeshwar369@gmail.com">`;
 
-    // Add table headers
     html += `<thead><tr>`;
     headers.forEach(header => {
         html += `<th>${header}</th>`;
     });
     html += `</tr></thead>`;
 
-    // Add table rows
     html += `<tbody>`;
     data.forEach(row => {
         html += `<tr>`;
